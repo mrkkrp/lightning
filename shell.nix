0434664d908307af4470679d91205bfb7c825ec3
mrkkrp <markkarpov92@gmail.com>
2018-04-28 20:10:54 +0700

    Add ‘shell.nix’

---
+++ shell.nix
@@ -0,0 +1,32 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc842", doBenchmark ? false }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, containers, deepseq, parser-combinators
+      , stdenv
+      }:
+      mkDerivation {
+        pname = "lightning";
+        version = "0.0.1.0";
+        src = ./.;
+        libraryHaskellDepends = [
+          base containers deepseq parser-combinators
+        ];
+        homepage = "https://github.com/mrkkrp/lightning";
+        description = "Monadic parser combinators";
+        license = stdenv.lib.licenses.bsd3;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+
+  drv = variant (haskellPackages.callPackage f {});
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
